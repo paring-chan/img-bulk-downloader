545e967b7ba456f604f7b0c5ad7e42e9f7d545a7
파랑이 <parangee9706@gmail.com>
2021-07-12 14:56:37 +0900

    안쓸 예정인거 삭제

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 #!node
 const inquirer = require('inquirer')
-const fetch = require('node-fetch')
-const {JSDOM} = require("jsdom");
+// const fetch = require('node-fetch')
+// const {JSDOM} = require("jsdom")
+const puppeteer = require('puppeteer')
 
 setImmediate(async () => {
     await inquirer.prompt([
@@ -19,20 +20,26 @@ setImmediate(async () => {
             default: ''
         }
     ]).then(async (data) => {
-        const res = await fetch(data.url).then(x => x.text())
-        const dom = new JSDOM(res)
-        const images = Array.from(dom.window.document.querySelectorAll(data.selector)).map(x => Array.from(x.querySelectorAll('img')).map(x => x.src))
-        let iterator = 1
-        if (!require('fs').existsSync('download')) require('fs').mkdirSync('download')
-        for (const value of images) {
-            for (const image of value) {
-                let img = image
-                if (image.startsWith('//')) img = 'http:' + image
-                console.log(`Downloading: ${img}`)
-                const buffer = await fetch(img).then(x => x.buffer())
-                require('fs').writeFileSync(require('path').join(process.cwd(), 'download', data.prefix + iterator + '.' + img.split('.').pop()), buffer)
-                iterator++
-            }
-        }
+        // const res = await fetch(data.url).then(x => x.text())
+        // const dom = new JSDOM(null, {
+        //     url: data.url,
+        //     runScripts: 'outside-only'
+        // })
+        // dom.window.addEventListener('DOMContentLoaded', () => {
+        //     const images = Array.from(dom.window.document.querySelectorAll(data.selector)).map(x => Array.from(x.querySelectorAll('img')).map(x => x.src))
+        //     console.log(images)
+        //     // let iterator = 1
+        //     // if (!require('fs').existsSync('download')) require('fs').mkdirSync('download')
+        //     // for (const value of images) {
+        //     //     for (const image of value) {
+        //     //         let img = image
+        //     //         if (image.startsWith('//')) img = 'http:' + image
+        //     //         console.log(`Downloading: ${img}`)
+        //     //         const buffer = await fetch(img).then(x => x.buffer())
+        //     //         require('fs').writeFileSync(require('path').join(process.cwd(), 'download', data.prefix + iterator + '.' + img.split('.').pop()), buffer)
+        //     //         iterator++
+        //     //     }
+        //     // }
+        // })
     })
 })
